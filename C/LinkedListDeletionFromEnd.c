ab7c3d37f28d396e79f1689a9a607e5adc7e011e
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-10-26 23:50:49 +0530

    Deletion from end

---
+++ C/LinkedListDeletionFromEnd.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+struct node
+{
+    int data;    //To hold Data
+    struct node *next;  //Pointer to Next Node
+};
+/*Some data has to be present to show insertion at any position.... So..*/
+void append(struct node **head,int x) //appends to headnode.....
+{
+    struct node *newnode,*temp;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = x;
+    newnode->next = NULL;
+    temp = (*head);
+    if((*head)==NULL)
+    {
+        *head = newnode;
+    }
+    else
+    {
+        while(temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;   
+    }
+}
+void deleteend(struct node **head)
+{
+    struct node *temp;
+    if((*head)==NULL)
+    {
+        return;
+    }
+    if((*head)->next==NULL)
+    {
+        free(*head);
+        return;
+    }
+    temp = (*head);
+    while(temp->next->next!=NULL)
+    {
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next = NULL;
+}
+
+void print(struct node **head) //Prints the Data
+{
+    struct node *temp = (*head);
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    struct node *head = NULL; //Set to null as append would assign the rest of the nodes... at the end
+    int n = 5,x,pos;
+    for(int i=1;i<=n;i++)
+    {
+        append(&head,i); //can be any... using scanf too..
+    }
+    print(&head);
+    printf("Enter no of elements to Delete \n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        deleteend(&head);
+        print(&head);
+    }
+    return 0;
+}
