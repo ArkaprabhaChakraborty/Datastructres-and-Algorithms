642b8956d4ed9bc8a9ec830265f48d846d85fbba
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-11-01 20:13:02 +0530

    itertools combination added

---
+++ Python/itertoolsCombination.py
@@ -0,0 +1,15 @@
+'''
+itertools.combinations(iterable, r)
+This tool returns the r length subsequences of elements from the input iterable.
+Combinations are emitted in lexicographic sorted order. 
+So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
+'''
+
+from itertools import combinations
+s,x = input().split()
+x = int(x)
+for i in range(x):
+   h = combinations(sorted(s),i+1)
+   for j in h:
+    r = ''.join(j)
+    print(r)
