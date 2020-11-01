744ecaea6e96bf1e3e1a624967d43e62345d39bf
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-11-01 19:05:06 +0530

    Super set check added

---
+++ Python/itertoolsproduct.py
@@ -0,0 +1,7 @@
+'''
+itertools.product()
+
+This tool computes the cartesian product of input iterables.
+It is equivalent to nested for-loops.
+For example, product(A, B) returns the same as ((x,y) for x in A for y in B).
+'''
