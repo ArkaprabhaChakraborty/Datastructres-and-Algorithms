bd4438d7c5ad90ecd726ecb5003e431a4102d373
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-11-01 20:16:18 +0530

    itertools combination

--- Python/itertoolsCombination.py
+++ Python/itertoolsCombination.py
@@ -3,6 +3,9 @@ itertools.combinations(iterable, r)
 This tool returns the r length subsequences of elements from the input iterable.
 Combinations are emitted in lexicographic sorted order. 
 So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
+
+You are given a string S.
+Your task is to print all possible combinations, up to size k, of the string S in lexicographic sorted order.
 '''
 
 from itertools import combinations
