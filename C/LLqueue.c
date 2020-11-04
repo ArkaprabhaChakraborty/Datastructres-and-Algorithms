b215beb5315cd22c8cfb8349b4608cc1646d178a
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-11-05 00:45:04 +0530

    Queue using Linked List added

--- C/LLqueue.c
+++ C/LLqueue.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
-
+#include<stdbool.h>
 struct node   //LinkedList node-struct
 {
     int data;
@@ -12,6 +12,9 @@ struct queue //queue-struct... required for fifo movement
     struct node *front;
     struct node *rear;
 };
+//Basically this queue struct has two node pointers.
+/* 1)front: pointing to the first node of the Linked list
+   2)rear: pointing to the last node of the Linked list */ 
 struct queue *createqueue() //utility function allocating memory for the front and rear structure pointer
 {
     struct queue *q = (struct queue*)malloc(sizeof(struct queue));
@@ -37,21 +40,75 @@ void enqueue(struct queue **q,int k) // k will be inserted to the queue using a 
 }
 void print(struct queue **q)
 {
-    struct queue *temp = *q;
+    struct node *temp = (*q)->front;
     while(temp != NULL)
     {
-        printf("%d ",temp->front->data);
-        temp->front = temp->front->next;
+        printf("%d ",temp->data);
+        temp = temp->next;
     }
+    printf("\n");
+}
+int len(struct queue **q)
+{
+    int c = 0;
+    struct node *temp = (*q)->front;
+    while(temp != NULL)
+    {
+        c = c + 1;
+        temp = temp->next;
+    }
+    return c;
+}
+void dequeue(struct queue **q)
+{
+    if((*q)->front == NULL)
+    {
+        (*q)->rear = NULL;
+        return;
+    }
+    struct node *temp = (*q)->front;
+    printf("%d deleted \n",temp->data);
+    (*q)->front = (*q)->front->next;
+    free(temp);
 }
 int main()
 {
     struct queue *q = createqueue();
-    for(int i=1;i<=5;i++)
+    bool loop = true;
+    while(loop)
     {
-        enqueue(&q,i);
+        printf("\nQueue Operations\n1)To insert elements\n2)To remove elements\nAny other to exit\n \nEnter your choice: ");
+        int c,n,x;
+        scanf("%d",&c);
+        switch(c)
+        {
+            case 1:printf("Enter No of elements \n");
+                  scanf("%d",&n);
+                  printf("enter elements \n");
+                  for(int i=1;i<=n;i++)
+                  {
+                    scanf("%d",&x);
+                    enqueue(&q,x);
+                  }
+                  printf("Queue Formed\n");
+                  print(&q);
+            break;
+            case 2:printf("Enter No of elements \n");
+                  scanf("%d",&n);
+                  if(n<=len(&q))
+                  {
+                      for(int i=1;i<=n;i++)
+                      {
+                          dequeue(&q);
+                      }
+                  }
+                  printf("New Queue\n");
+                  print(&q);
+            break;
+            default:loop=false;
+            break;
+        }
     }
-    print(&q);
     return 0;
 }
 
