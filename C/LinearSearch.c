8093cb4df337838e9611e85d60836c2075df47b6
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-03-29 00:36:18 +0530

    searches added

--- C/LinearSearch.c
+++ C/LinearSearch.c
@@ -28,7 +28,7 @@ int main()
     }
     printf("Enter key to be searched: ");
     scanf("%d",&k);
-    k = binarySearch(arr,0,n,k);
+    k = LinearSearch(arr,n,k);
     if(k!=-1)
     {
         printf("element found at position %d",k);
