f1affdbafb610e7cf343c0bf1b7930973ede34e7
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-05-12 02:35:34 +0530

    Topsort added

--- Python/TopologicalSort.py
+++ Python/TopologicalSort.py
@@ -14,28 +14,37 @@ class Graph:
             print("{}".format(i),end = " ")
             y = self.graph[i]
             for j in y:
-                print("-> {} , ".format(j),end=" ")
+                print("{}-> {} ".format(i,j),end=" ")
             print()
 
 
-    def dfs_util(self,visited,v,tsort):
-        visited.add(v)
-        n = self.graph[v]
-        if (len(n) == 0) or (v not in tsort):
-            tsort.append(v)
+    def dfs(self,vnodes,v,visited):
+        visited[v] == True
+        n = self.graph[self.vertices[v]]
         for i in n:
-            if i not in visited:
-                self.dfs_util(visited,i,tsort)
+            x = self.vertices.index(i)
+            if visited[x] == False:
+                self.dfs(vnodes,x,visited)
+        vnodes.append(v)
 
-    def dfs(self,v):
-        visited = set()
-        tsort = []
-        while len(visited) != len(self.vertices):
-            self.dfs_util(visited,v,tsort)
-        print(tsort)
+    def topological_sort(self):
+        visited = [False]*len(self.vertices)
+        tsort = [0] * len(self.vertices)
+        j = len(self.vertices) - 1
+        for i in range(len(self.vertices)):
+            if visited[i] == False:
+                vnodes  = []
+                self.dfs(vnodes,i,visited)
+                for x in vnodes:
+                    tsort[j] = x
+                j = j - 1
+        tsort = tsort[::-1]
+        for i in tsort:
+            print("{} ".format(self.vertices[i]), end=" ")
+        print()
 
 
-print("Enter Vertices list")
+print("Enter Vertices list (begining with the leaf nodes to get a root->leaf arrangement and with root to get a leaf->root arrangement)")
 x = list(input().split())
 
 graph = Graph(x)
@@ -50,9 +59,8 @@ while True:
 print("The adjacency list of the graph is as follows: ")
 graph.print_graph()
 
-print("Enter vertex number to start Topological sort")
-x  = input()
+
 
 print("Topological arrangement is as follows for the graph is as follows:")
-graph.dfs(x)
+graph.topological_sort()
 
