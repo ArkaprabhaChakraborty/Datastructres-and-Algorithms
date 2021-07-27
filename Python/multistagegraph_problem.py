b1d7bcce0d562afaabaa24f1abc5131c023d4d06
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-07-28 05:08:19 +0530

    multistage graph problem solved

--- Python/multistagegraph_problem.py
+++ Python/multistagegraph_problem.py
@@ -4,30 +4,50 @@ Here we break the graph representation into different stages.
 We define a function M(i,j) = dist[j] + M(i+1,j) 
 '''
 
-
-
-def shortestDist(graph,N):
+def get_min(s,n,graph,cost):
     INF = 10000000000
-    dist = [0] * N
-    dist[N - 1] = 0
-    vert = []
-    for i in range(N - 2, -1, -1):
-        dist[i] = INF
-        for j in range(N):
-            if graph[i][j] == INF:
-                continue
-            dist[i] = min(dist[i],graph[i][j] + dist[j])
-    return dist[0]
+    min_cost = INF
+    min_vert = 0
+    for i in range(n):
+        if (min_cost>(graph[s][i] + cost[i])):
+            min_cost = (graph[s][i] + cost[i])
+            min_vert = i
+    return min_vert
+
+
+def multistage_shortest_path(graph,n,stage):
+    d = [0]*n
+    cost = [0]*n
+    for i in range(n-2,-1,-1):
+        r = get_min(i,n,graph,cost)
+        cost[i] = graph[i][r] + cost[r]
+        d[i] = r
+
+    p = [0]*stage
+    p[stage-1] = n-1
+    for i in range(1,stage-1):
+        p[i] = d[p[i-1]]
+
+    print(f"Cost is {cost[0]}")
+    print("Path:")
+    print_path(p)
+    
+def print_path(path):
+    print(path[0],end=" ")
+    for i in path[1:len(path)]:
+        print(f"-> {i}",end=" ")
+    print()
 
 
 
 INF = 10000000000
-graph = [[INF, 1, 2, 5, INF, INF, INF, INF],
-         [INF, INF, INF, INF, 4, 11, INF, INF],
-         [INF, INF, INF, INF, 9, 5, 16, INF],
-         [INF, INF, INF, INF, INF, INF, 2, INF],
+graph = [[INF,  1,   2,  5,   INF, INF, INF, INF],
+         [INF, INF, INF, INF,  4,  11, INF, INF],
+         [INF, INF, INF, INF, 9,    5,   16, INF],
+         [INF, INF, INF, INF, INF, INF,   2, INF],
          [INF, INF, INF, INF, INF, INF, INF, 18],
          [INF, INF, INF, INF, INF, INF, INF, 13],
-         [INF, INF, INF, INF, INF, INF, INF, 2]] 
-print(shortestDist(graph,8))
+         [INF, INF, INF, INF, INF, INF, INF, 2]]
+          
+multistage_shortest_path(graph,8,4)
 
